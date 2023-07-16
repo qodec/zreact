71af7bf0f3dc31deb69b502490db316a83e22a09
Charles Obimnaeto Egesionu <105589308+IamNaeto@users.noreply.github.com>
2023-07-16 12:11:57 +0000

    update the about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-
+import logo from './about-us.png'; // with import
 export default function About() {
   return (
     <>
-      <div>About Page</div>
-      <div className="p-10 text-green">This will contain something about the about page in the nearest future</div>
-      <div className="flex ">
-        <div className="bg-red h-40 w-70">picture 1</div>
-        <div className="bg-orange h-40 w-70">picture 2</div>
+      <div className="flex align-center justify-between py-10px flex-col" >
+        <h1 className="text-blue text-center" >About Us</h1>
+        <div className="flex align-center justify-between">
+          <div className="mx-20px">
+            <img src={logo} alt="" className="w-500px"/>
+          </div>
+          <div className="w-100% px-20px">
+            <p className="lh-24px text-justify">We are visionaries, we are developers, we are software enginners. We learn by doing and we are exploring the world of technology through frontend development, backend development, software enginnering and deVops. We contribute to orgaanisational progress via hard work, resistancy and dedication. We are Zreact!!!!</p>
+            <button className="px-15 py-10px bg-blue text-white b-blue">Explore</button>
+          </div>
+        </div>
       </div>
     </>
   );
