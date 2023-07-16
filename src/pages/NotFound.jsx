3dae1f093a411edee08dc752ebb4cb439a08a644
Charles Obimnaeto Egesionu <105589308+IamNaeto@users.noreply.github.com>
2023-07-16 12:22:44 +0000

    modified error page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import error from './error.png';
 
 export default function Notfound() {
   return (
     <>
-      <div>404 Page</div>
-      <div className="p-10 text-green">Try again</div>
-      <div className="flex ">
-        <div className="bg-red h-40 w-70">Not</div>
-        <div className="bg-orange h-40 w-70">Found</div>
+     <div className="flex align-center justify-between py-10px flex-col" >
+        <div className="flex align-center justify-between">
+          <div className="mx-20px">
+            <img src={error} alt="" className="w-500px"/>
+          </div>
+          <div className="w-100% px-20px">
+            <h1 className="text-blue text-center" >ERROR 404</h1>
+            <p className="lh-24px text-justify">Unforunatly you ran into a problem, please click on the button below to redirect back to the previous page</p>
+            <button className="px-15 py-10px bg-blue text-white b-blue">Redirect Back</button>
+          </div>
+        </div>
       </div>
     </>
   );
