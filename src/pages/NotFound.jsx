a03e7d51df6842b149421b517b51d874c182c7d3
Charles Obimnaeto Egesionu <105589308+IamNaeto@users.noreply.github.com>
2023-07-16 12:28:44 +0000

    more update on the error page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -9,7 +9,7 @@ export default function Notfound() {
           <div className="mx-20px">
             <img src={error} alt="" className="w-500px"/>
           </div>
-          <div className="w-100% px-20px">
+          <div className="w-100% px-20px flex flex-col my-150px" >
             <h1 className="text-blue text-center" >ERROR 404</h1>
             <p className="lh-24px text-justify">Unforunatly you ran into a problem, please click on the button below to redirect back to the previous page</p>
             <button className="px-15 py-10px bg-blue text-white b-blue">Redirect Back</button>
