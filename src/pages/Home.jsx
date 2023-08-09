bbdd241509d726963c5bcaa4b3fe1db215958699
L eye <leyume@yahoo.com>
2023-08-09 09:18:07 +0100

    Some updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
 
 export default function Home() {
-  let [inputText, setInputText] = useState("I am AWESOME");
-  let [bannerText, setBannerText] = useState("I am AWESOME");
+  let [inputText, setInputText] = useState("I am Fantastico");
+  let [bannerText, setBannerText] = useState("I am Fantastico");
 
   useEffect(() => {
     console.log("Jane Jane Jane Jane Jane ....");
@@ -14,8 +14,8 @@ export default function Home() {
     <div>
       <section className="banner bg-black text-white text-4xl min-h-200px text-center items-center flex justify-center">{bannerText}</section>
       <section id="features" className="bg-gray-300 py-6 text-center">
-        <h2 className="text-3xl opacity-50 p-0 m-0">Hey Homie,</h2>
-        <div>tell us how you feel</div>
+        <h2 className="text-3xl opacity-50 p-0 m-0">Hello Hey Hi,</h2>
+        <div>Tell me how you feel</div>
         <input type="text" onChange={handleChange} className="block mx-auto text-xl p-2 my-2 text-center" />
         <button onClick={() => setBannerText(inputText)} className="px-4 py-1 uppercase">
           Enter
